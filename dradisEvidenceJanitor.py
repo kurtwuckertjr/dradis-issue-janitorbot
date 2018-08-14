880be840f2e640c1c01d3367a44a1b164b71c281
Shane Scott <sscott@gvit.com>
2018-08-14 06:59:19 -0500

    Pushing revised issue janitor

---
+++ dradisEvidenceJanitor.py
@@ -0,0 +1,187 @@
+from pydradis3 import Pydradis3
+from json import dumps
+from sys import argv, exit, version
+from argparse import ArgumentParser
+import time
+import datetime
+import re
+
+#### Not even started! ####
+class EvidenceJanitorScript(object):
+    def __init__(self):
+        self.arg = self.processArguments()
+        if len(argv) != 4:
+            print("Possibly missing arguments. Try HELP")
+            exit(-6)
+        # Dradis API Configuration
+        self.verifyCert = True    # change this to make requests without verifying
+        self.dradisApiToken = self.arg.dradisApiToken
+        self.dradisProjectId = self.arg.dradisProjectId
+        self.dradisUrl = self.arg.dradisUrl
+        self.dradisDebug = False
+        self.dradisSession = Pydradis3(self.dradisApiToken, self.dradisUrl, self.dradisDebug, self.verifyCert)
+    
+    def run(self):
+        try:
+            self.issueCleaner(self.dradisProjectId)
+        except Exception as e:
+            print('Failed in run: {0}'.format(e))
+            exit(-1)
+        self.dradisSession = None
+        return 0
+
+    def stripTrash(self, dirtyText: str):
+        sanitizedText = dirtyText.replace('"','&quot;').replace("'","&apos;").replace("<","&lt;").replace(">","&gt;").replace("&","&amp;")
+        return sanitizedText
+
+    def issueCleaner(self, projectId):
+        # Remove bad XML characters from Dradis issues
+        today = str(datetime.datetime.now())
+        newCveSite = 'https://cvedetails.com/cve/'
+        refPattern = re.compile(r'References')
+        cvePattern = re.compile(r'CVE_ID')
+        cwePattern = re.compile(r'CWE_ID')
+        modulePattern = re.compile(r'Module_OTGv4')
+        touchedByPattern = re.compile(r'Touched_By')
+
+        issueList = self.dradisSession.get_issuelist(pid=projectId)
+
+        # Loop over every primary issue (highest level JSON object) in the dict containing all Dradis issues
+        for issueEntry in issueList:
+            issueId = issueEntry[1]
+            print("Sanitizing issue {0}...".format(issueId))
+            issue = self.dradisSession.get_issue(pid=projectId, issue_id=issueId)
+            sanitizedFields = []
+            # Loop over every field and value within each Dradis issue
+            issueTitle = issueModules = issueText = frankenstein = '' 
+            cweField = '\r\n#[CWE_ID]#\r\n'
+            cveField = '\r\n#[CVE_ID]#\r\n'
+            endOfIssue = realEndOfIssue = 0
+            for key, value in issue.items():
+                # Storing current issue title, for debugging
+                if key == 'title':
+                    issue_title = self.stripTrash(str(issue[key]))
+                    issueTitle = "#[Title]#\r\n" + issue_title + "\r\n\r\n"
+
+                # Appending the entirety of the current issue into a string variable
+                if key == 'text':
+                    issueText = self.stripTrash(str(issue[key]))
+                    realEndOfIssue = len(issueText)
+                    myEnd = re.search(modulePattern, issue[key])
+                    touchMe = re.search(touchedByPattern, issue[key])
+                    if not touchMe:
+                        if myEnd:
+                            x = myEnd.start() - 2
+                            y = myEnd.end() + 2
+                            endOfIssue = len(issueText) - x
+                            issueModules = myEnd.string[x:]
+                        else:
+                            issueModules = "#[Module_OTGv4]#\r\n\r\n#[Module_PCIDSS32]#\r\n\r\n#[Module_HIPAA]#\r\n\r\n#[Module_NIST53]#\r\n\r\n#[Module_ISO27K2013]#\r\n\r\n#[Module_GDPR2016]#\r\n\r\n#[Touched_By]#\r\nCVE_Bot_" + today + "\r\n\r\n"
+                    else:
+                        a = touchMe.start() - 2
+                        b = touchMe.end() + 2
+                        if myEnd:
+                            y = myEnd.start() - 2
+                            z = myEnd.end() + 2
+                            issueModules = myEnd.string[y:z]
+                        else:
+                            issueModules = "#[Module_OTGv4]#\r\n\r\n#[Module_PCIDSS32]#\r\n\r\n#[Module_HIPAA]#\r\n\r\n#[Module_NIST53]#\r\n\r\n#[Module_ISO27K2013]#\r\n\r\n#[Module_GDPR2016]#\r\n\r\n"
+
+                # 'Fields' is a JSON parameter that contains its own JSON parameters, so it is its own dict. Loop over it to dig inside each issue.
+                if key == 'fields':
+                    fields = value
+                    # Looping over every key and value of the fields dict
+                    for cey, falue in fields.items():
+                        sanitizedFormattedValue = "#[{0}]#\r\n{1}\r\n\r\n".format(cey, self.stripTrash(str(fields[cey])))
+                        if cey != 'Touched_By':
+                            sanitizedFields.append(sanitizedFormattedValue)
+                        else:
+                            sanitizedFields.append("#[{0}]#\r\nJanitor_Bot_{1}\r\n\r\n".format(cey, today))
+
+            sanitizedIssueText = ''.join(str(listEntry) for listEntry in sanitizedFields)
+
+            # This is the only way to do PUT or POST requests with Dradis API
+            data = {'issue': {"text": sanitizedIssueText}}
+            issueUpdate = self.dradisSession.update_issue_raw(pid=projectId, issue_id=issueId, data=data)
+        return
+
+    def evidenceCleaner(self):
+        # Removing bad XML characters from evidence in Dradis project
+        # HTTP GET request headers
+        headers = {'Authorization': 'Token token={0}'.format(self.dradis_api_token), 'Dradis-Project-Id': self.dradis_project_id}
+
+		# HTTP PUT request headers
+        putHeaders = {'Authorization': 'Token token={0}'.format(self.dradis_api_token), 'Dradis-Project-Id': self.dradis_project_id, 'Content-Type':'application/json'}
+
+        # HTTP GET request to get all issues in the specified Dradis project
+        response = self.session.get(self.dradis_evidence_url, headers=headers, verify=self.verify_cert)
+
+        # If the above GET request returns 200 code, let the user know, otherwise say what's wrong
+        if '[200]' in str(response):
+            print('HTTP 200 OK')
+        else:
+            print('Did not receive HTTP 200 code, probably incorrect Dradis url argument. ' + str(response))
+        
+        # Convert the GET response into a JSON object which will be interpreted by Python as a dict, lovely
+        evidences = response.json()
+
+        # Loop over every primary issue (highest level JSON object) in the dict containing all Dradis issues
+        for evidence in evidences:
+            # Loop over every field and value within each Dradis evidence
+            issueResult = ''
+            issueTitle = ''
+            issue_id = ''
+            evidenceText = ''
+            evidenceResult = ''
+            for key, value in evidence.items():
+                # Code to find issue ID, which is used to identify the issue and is used in the PUT URL, very important
+                if key == 'id':
+                    issue_id = str(issue[key])
+                    dradis_issue_url = self.dradis_issues_url + '/' + issue_id
+
+                # Storing current issue title, for debugging
+                if key == 'title':
+                    issue_title = self.stripTrash(str(issue[key]))
+                    issueTitle = "#[Title]#\r\n" + issue_title + "\r\n\r\n"
+                #print('KEY: ' + str(key) + ' VALUE: ' + str(value))  # Listing every issue field and value just to show that it is working
+
+                # Appending the entirety of the current issue into a string variable
+                if key == 'text':
+                    evidenceText = self.stripTrash(str(evidence[key]))
+
+                # 'Fields' is a JSON parameter that contains its own JSON parameters, so it is its own dict. Loop over it to dig inside each issue.
+                if key == 'fields':
+                    fields = value
+                    # Looping over every key and value of the fields dict
+                    for cey, falue in fields.items():
+                        if cey == 'Result':
+                            evidenceResult = "#[Result]#\r\n" + self.stripTrash(str(fields[cey])) + "\r\n\r\n"
+                            
+            frankenstein = evidenceResult
+
+            # This is the only way to do PUT or POST requests with Dradis API
+            issue_data = {'issue': {"text": frankenstein}}
+
+            # The almighty HTTP PUT request to edit issues
+            dradis = requests.put(dradis_evidence_url, data=dumps(issue_data), headers=putHeaders, verify=self.verify_cert)
+            if dradis.status_code == 200:
+                print("Successfully cleaned up evidence for the following issue:  " + issue_title)
+            else:
+                print("Failed cleaning up evidence for the following issue: \n\n{1}\nStatus Code: {2}\n\n".format(dradis.status_code, dradis.text))
+        return
+
+    def processArguments(self):
+        # parse the arguments
+        parser = ArgumentParser(epilog='\tExample: \r\npython ' + argv[0] +
+                                       "https://dradis-pro.dev 21 xa632ghas87d393287",
+                                description="Remove bad XML characters from Dradis issues")
+        parser.add_argument('dradisUrl', help="Dradis URL")
+        parser.add_argument('dradisProjectId', help="Dradis Project ID")
+        parser.add_argument('dradisApiToken', help="Dradis API token")
+        return parser.parse_args()
+
+if __name__ == "__main__":
+    start_time = time.time()
+    scriptInstance = EvidenceJanitorScript()
+    scriptInstance.run()
+    print("\n\n%s seconds" % (time.time() - start_time))
---
+++ testScript.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+/root/miniconda3/bin/python3 dradisIssueJanitor.py "https://dradispro.govanguard.co" "7" "gnTM4gPnTF6pbkQgYXPi9mH2"
